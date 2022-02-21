c2a8d46e0ca73b3900ad9fde3e620aa212432029
Igor Nepipenko <inepipenko@muellerone.com>
2022-02-21 20:47:20 +0200

    feat(no-ref): start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
 	selector: 'ngx-classwork-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css'],
+	// encapsulation: ViewEncapsulation.Emulated,
 })
 export class AppComponent {
-	public title = 'project-ngx-classwork-starter';
+	public title = 'My salary is';
+
+	public titleClass = 'title';
+
+	public imgSrc =
+		'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/800px-Angular_full_color_logo.svg.png';
+
+	public tag = this.domSanitizer.bypassSecurityTrustHtml('<span style="color:red">Angular</span>');
+
+	public width = 50;
+
+	private readonly salary = 3000;
+
+	private readonly bonus = 1.2;
+
+	public user: any = {
+		name: 'Ihor',
+	};
+
+	public constructor(private readonly domSanitizer: DomSanitizer) {}
+
+	public getSalary() {
+		return Math.round(this.salary * this.bonus);
+	}
+
+	public getImgSrc(src: string, event: MouseEvent) {
+		console.log(src);
+		console.log(event);
+	}
+
+	public searchText(text: string, myDiv: HTMLElement) {
+		console.log(text);
+		console.log(myDiv);
+		// public searchText(event: Event, myInput: any) {
+		// const value = (event.target as HTMLInputElement).value;
+		// console.log(value);
+		// console.log(myInput);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from '../environments/environment';
+
+//@ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const baseURL = environment.baseUrl;
+
+/**
+ *  NgModule => es6
+ *  declarations => let/const
+ *  imports => import
+ *  exports = export
+ */
 
 @NgModule({
 	declarations: [AppComponent],
 	imports: [BrowserModule, BrowserAnimationsModule],
-	providers: [],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
